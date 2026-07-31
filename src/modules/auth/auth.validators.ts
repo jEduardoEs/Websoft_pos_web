bf739634aff584d21418314f4f07f366b47552fe
Angel Sanchez <angelgustavos93@gmail.com>
2026-07-31 16:41:27 -0600

    Borramos duplicados

--- src/modules/auth/auth.validators.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const credentialsSchema = z.object({
-  usuario: z.string().nonempty({ message: 'Usuario es requerido' }),
-  password: z.string().nonempty({ message: 'Contraseña es requerida' }),
-})
-
-export type CredentialsSchema = z.infer<typeof credentialsSchema>
